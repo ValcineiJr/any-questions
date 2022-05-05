56ee7ccc12772ac19c8d945931e2d6440c1314b9
Valcinei Jr <hakjog@gmail.com>
2022-05-05 17:39:06 -0300

    trocando importação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import cx from 'classNames';
+import cx from 'classnames';
 import { Container } from './styles';
 import Link from 'next/link';
 
